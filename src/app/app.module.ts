ca54dbe1784d4dcf9bbeb5f77c6d2e8da7941ffa
Alex <alex.rigueur@gmail.com>
2020-09-09 11:07:46 +0200

    add login register footer & navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './app/components/user/login/login.component';
+import { RegisterComponent } from './app/components/user/register/register.component';
+import { NavbarComponent } from './app/components/common/navbar/navbar.component';
+import { FooterComponent } from './app/components/common/footer/footer.component';
+import { HomeComponent } from './app/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
